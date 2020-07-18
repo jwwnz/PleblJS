e20f9951b97b2eef388b0e1f8903a7dec2947e89
jwwnz <jwon117@aucklanduni.ac.nz>
2020-07-18 20:28:49 +1200

    Add month calculations for DWT calculator

--- src/pages/DWT.jsx
+++ src/pages/DWT.jsx
@@ -5,9 +5,14 @@ import "./HomePage.css";
 const DWTPage = () => {
 	const [price, setPrice] = useState(0);
 	const [hours, setHours] = useState(0);
+	const [time, setTime] = useState("week");
 
 	const handlePriceChange = (event) => {
 		setPrice(event.target.value);
+	};
+
+	const handleTimeChange = (event) => {
+		setTime(event.target.value);
 	};
 
 	const handleHoursChange = (event) => {
@@ -24,15 +29,22 @@ const DWTPage = () => {
 				<h2>So you wanna waste time?</h2>
 				<Form>
 					<Form.Group controlId="formPrice">
-						<Form.Label>What did you pay per week</Form.Label>
+						<Form.Label>What do you pay?</Form.Label>
 						<Form.Control
 							type="number"
-							placeholder="$ per week"
+							placeholder="$"
 							onChange={handlePriceChange}
 						/>
 						<Form.Text className="text-muted">
 							Lets get to the truth shall we...
 						</Form.Text>
+					</Form.Group>
+					<Form.Group controlId="exampleForm.ControlSelect2">
+						<Form.Label>How frequently do you pay?</Form.Label>
+						<Form.Control as="select" onChange={handleTimeChange}>
+							<option>week</option>
+							<option>month</option>
+						</Form.Control>
 					</Form.Group>
 
 					<Form.Group controlId="formHours">
@@ -54,9 +66,11 @@ const DWTPage = () => {
 				<hr />
 				{price > 0 && hours > 0 && (
 					<div>
-						You paid ${price}, for {hours} hours, which means the rate you are
-						paying is&nbsp;
-						<strong>${price / hours} per hour</strong>
+						You paid ${price} per {time}, for {hours} hours, which means the
+						rate you are paying is&nbsp;
+						<strong>
+							${(time === "week" ? price : price / 4) / hours} per hour
+						</strong>
 					</div>
 				)}
 			</Container>
